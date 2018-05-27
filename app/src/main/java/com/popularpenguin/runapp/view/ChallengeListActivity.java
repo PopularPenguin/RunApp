3bc0a0f9900288b4a2c16e95e32c2c67f09ba517
Brian Niedzialkoski <brianniedzialkoski@gmail.com>
2018-05-27 16:47:08 -0400

    Improve requesting permissions

--- app/src/main/java/com/popularpenguin/runapp/view/ChallengeListActivity.java
+++ app/src/main/java/com/popularpenguin/runapp/view/ChallengeListActivity.java
@@ -10,6 +10,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.ContextCompat;
@@ -94,15 +95,12 @@ public class ChallengeListActivity extends AppCompatActivity implements
         super.onResume();
 
         checkIfGpsEnabled();
-        checkPermissions();
     }
 
     private void checkIfGpsEnabled() {
         LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            // TODO: Make a snackbar 'Location services must be enabled for this app'
-
             enableGps();
         }
     }
@@ -140,6 +138,17 @@ public class ChallengeListActivity extends AppCompatActivity implements
 
     @Override
     public void onClick(int position) {
+        checkPermissions();
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            Snackbar.make(mAppBar, R.string.snackbar_location_services, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.snackbar_dismiss, view -> {})
+                    .show();
+
+            return;
+        }
+
         Intent intent = new Intent(this, ChallengeActivity.class);
         intent.putExtra(RunTracker.CHALLENGE_BUNDLE_KEY, mChallengeList.get(position));
 
